c3d37c99fd9e868a3a0fb0acf511f20e95170c5e
pak <neal0907@hanyang.ac.kr>
2026-06-08 19:42:09 +0900

    offline_sac

--- src/holosoma/holosoma/config_values/experiment.py
+++ src/holosoma/holosoma/config_values/experiment.py
@@ -2,7 +2,7 @@ import tyro
 from typing_extensions import Annotated
 
 from holosoma.config_types.experiment import ExperimentConfig
-from holosoma.config_values.loco.g1.experiment import g1_29dof, g1_29dof_fast_sac
+from holosoma.config_values.loco.g1.experiment import g1_29dof, g1_29dof_fast_sac, g1_29dof_offline_sac
 from holosoma.config_values.loco.t1.experiment import t1_29dof, t1_29dof_fast_sac
 from holosoma.config_values.wbt.g1.experiment import (
     g1_29dof_wbt,
@@ -23,6 +23,7 @@ from holosoma.config_values.wbt.g1.experiment import (
 DEFAULTS = {
     "g1_29dof": g1_29dof,
     "g1_29dof_fast_sac": g1_29dof_fast_sac,
+    "g1_29dof_offline_sac": g1_29dof_offline_sac,
     "t1_29dof": t1_29dof,
     "t1_29dof_fast_sac": t1_29dof_fast_sac,
     "g1_29dof_wbt": g1_29dof_wbt,
