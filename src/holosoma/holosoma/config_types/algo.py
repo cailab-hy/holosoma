1240fc697ea3a3094e65ad6ba6eec7f8a534c3ec
pak <neal0907@hanyang.ac.kr>
2026-06-26 13:39:03 +0900

    clean cql

--- src/holosoma/holosoma/config_types/algo.py
+++ src/holosoma/holosoma/config_types/algo.py
@@ -493,7 +493,7 @@ class CQLConfig:
     critic_learning_rate: float = 3e-4
     """the learning rate of the critic"""
 
-    actor_learning_rate: float = 1e-4
+    actor_learning_rate: float = 3e-4
     """the learning rate for the actor"""
 
     alpha_learning_rate: float = 3e-4
